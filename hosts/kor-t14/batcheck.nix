14b16d4820d74ec9417af46702676380962a5f9d
erdfern <rexsomnia@pm.me>
2025-05-05 12:46:29 +0000

    --wip--

--- hosts/kor-t14/batcheck.nix
+++ hosts/kor-t14/batcheck.nix
@@ -1,24 +1,25 @@
 { pkgs, ... }:
 let
+  battery_path  = "/sys/class/power_supply/BAT0/capacity";
   batcheck = pkgs.writeShellScript "batcheck" ''
     #!/usr/bin/env bash
     
     set -eou pipefail
 
     # --- Configuration ---
-    BATTERY_PATH="/sys/class/power_supply/BAT0/capacity"
+    BATTERY_PATH=${battery_path}
     LOW_THRESHOLD="15" # Percentage threshold
     # --- ############ ---
 
-    notify-send "hi"
-
-    if [ ! -f "$BATTERY_PATH" ]; then
-      echo "Error: Battery path not found: $BATTERY_PATH" >&2
-      # notify-send -u critical "Battery Check Error" "Cannot find battery path: ''${BATTERY_PATH}"
-      exit 1
-    fi
+    # if [ ! -f "$BATTERY_PATH" ]; then
+    #   echo "Error: Battery path not found: $BATTERY_PATH" >&2
+    #   # notify-send -u critical "Battery Check Error" "Cannot find battery path: ''${BATTERY_PATH}"
+    #   exit 1
+    # fi
 
     CAPACITY=$(cat "$BATTERY_PATH")
+
+    notify-send "hi -u critical ''${CAPACITY}"
 
     if ! [[ "$CAPACITY" =~ ^[0-9]+$ ]]; then
         echo "Error: Invalid capacity value read: $CAPACITY" >&2
@@ -28,7 +29,7 @@ let
     if [ "$CAPACITY" -lt "$LOW_THRESHOLD" ]; then
       # -u critical: Sets urgency level
       # You can customize the title and message
-      /usr/bin/env notify-send -u critical \
+      notify-send -u critical \
                            "Battery level at ''${CAPACITY}%"
     fi
 
@@ -40,16 +41,12 @@ in
   systemd.user.services.battery-low-notif = {
     Unit = {
       Description = "Check battery level and notify if low";
-      # Documentation = [ "" ];
       PartOf = [ "graphical-session.target" ];
-      After = [ "graphical-session.target" ];
     };
     Service = {
       Type = "oneshot";
-      # BusName = "org.freedesktop.Notifications";
-      # ExecCondition = "/usr/bin/env sh -c '[ -n \"$WAYLAND_DISPLAY\" ]'";
+      ExecCondition = "/usr/bin/env sh -c '[ -f ${battery_path} ]'";
       ExecStart = batcheck;
-      # ExecReload = "/usr/bin/env makoctl reload";
     };
     Install.WantedBy = [ "graphical-session.target" ];
   };
@@ -61,10 +58,10 @@ in
       After = [ "graphical-session.target" ];
     };
     Timer = {
-      OnUnitActiveSec = "5s";
+      OnUnitActiveSec = "25s";
       # OnBootSec="15s";
       AccuracySec = "1s";
     };
-    Install.WantedBy = [ "timers.target" ];
+    Install.WantedBy = [ "timers.target" "graphical-session.target" ];
   };
 }
