55b2c8b7db6690e2adf32c9be1240243ad700909
erdfern <rexsomnia@pm.me>
2025-05-05 11:31:47 +0000

    --wip--

--- modules/home/desktop/default.nix
+++ modules/home/desktop/default.nix
@@ -27,18 +27,18 @@ in
     services.mako = {
       enable = true; # conflicts with hyprpanel/ags notif service
       # font = "Iosevka Nerd Font 12";
-      width = 256;
-      height = 500;
-      margin = "10";
-      padding = "5";
-      borderSize = 3;
-      borderRadius = 3;
       # backgroundColor = "#3A4353";
       # borderColor = "#c0caf5";
       # progressColor = "over #3B4252";
       # textColor = "#FAF4FC";
-      defaultTimeout = 5000;
       settings = {
+        defaultTimeout = 5000;
+        width = 256;
+        height = 500;
+        margin = "10";
+        padding = "5";
+        borderSize = 3;
+        borderRadius = 3;
         text-alignment = "center";
         # criteria."urgency=critical".border-color = "#B45C65";
       };
