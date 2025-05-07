1e06dac4029ef73a8c5d28e0129c8d416c578767
erdfern <rexsomnia@pm.me>
2025-05-07 11:56:39 +0000

    --wip--

--- packages/app2unit/default.nix
+++ packages/app2unit/default.nix
@@ -1,10 +1,16 @@
 { fetchurl
 , writeShellScriptBin
 }:
-let
-  src = fetchurl {
-    url = "https://raw.githubusercontent.com/Vladimir-csp/app2unit/42613bd4c69cd5720114679a52b73b8b5d947678/app2unit";
-    sha256 = "sha256-kYF3wzIcBOhxvNLE8AzI1VPQgJOfjh2qJQfmZqLG0Ss=";
-  };
-in
-writeShellScriptBin "app2unit" src
+fetchurl {
+  url = "https://raw.githubusercontent.com/Vladimir-csp/app2unit/42613bd4c69cd5720114679a52b73b8b5d947678/app2unit";
+  sha256 = "sha256-2PNk/G+5AfeFZm+7B8GkM9EhgyPw5A9U0hUblvsxdM8=";
+  executable = true;
+}
+# let
+#   src = fetchurl {
+#     url = "https://raw.githubusercontent.com/Vladimir-csp/app2unit/42613bd4c69cd5720114679a52b73b8b5d947678/app2unit";
+#     sha256 = "sha256-kYF3wzIcBOhxvNLE8AzI1VPQgJOfjh2qJQfmZqLG0Ss=";
+#     # executable = true;
+#   };
+# in
+# writeShellScriptBin "app2unit" src
