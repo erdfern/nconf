9b6b6a24a19e703afa02e9df6e30c1ef4802169a
erdfern <git@mail.erdfern.com>
2025-10-09 18:34:48 +0200

    --wip--

--- hosts/dns/default.nix
+++ hosts/dns/default.nix
@@ -4,6 +4,10 @@
     systems.nixos.dns = {
       # nixpkgs = config.inputs.nixpkgs-unstable;
       system = "aarch64-linux";
+      modules = [
+        # config.inputs.nixos-generators.result.nixosModules.sd-aarch64
+        config.inputs.nixos-generators.result.nixosModules.all-formats
+      ];
     };
   };
 }
