e25e84e90fc428a0e83939c9aa36eeac8e77869d
erdfern <git@mail.erdfern.com>
2025-10-09 15:19:00 +0200

    --wip--

--- anywhere/flake.nix
+++ anywhere/flake.nix
@@ -18,84 +18,66 @@
         (name: value: value.result)
         project.systems.nixos;
 
-      nodes = builtins.mapAttrs
+      configs = builtins.mapAttrs
         (name: value: {
+          system = value.pkgs.system;
+          specialArgs = value._module.specialArgs;
           modules = value._module.args.modules;
         })
         systems;
     in
     {
+      nixosConfigurations = configs // {
+        # dedi =
+        #   let
+        #     system = systems.dedi;
+        #   in
+        #   nixpkgs.lib.nixosSystem {
+        #     system = "x86_64-linux";
+        #     specialArgs = {
+        #       inherit inputs;
+        #     };
+        #     modules = system._module.args.modules;
+        #     # modules = system.modules;
+        #   };
 
-      nixosConfigurations.dedi =
-        let
-          system = systems.dedi;
-        in
-        nixpkgs.lib.nixosSystem {
+        # https://wiki.nixos.org/wiki/Install_NixOS_on_Hetzner_Cloud
+        hetzner-cloud = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
-          specialArgs = {
-            inherit inputs;
-          };
-          modules = system._module.args.modules;
-          # modules = system.modules;
-        };
-      nixosConfigurations.dns =
-        let
-          system = systems.dns;
-        in
-        nixpkgs.lib.nixosSystem {
-          system = "aarch64-linux";
-          specialArgs = {
-            inherit inputs;
-          };
-          modules = system._module.args.modules;
-          # modules = system.modules;
+          modules = [
+            disko.nixosModules.disko
+            ./configuration.nix
+          ];
         };
 
-      # https://wiki.nixos.org/wiki/Install_NixOS_on_Hetzner_Cloud
-      nixosConfigurations.hetzner-cloud = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-        ];
-      };
-
-      # nix run --extra-experimental-features 'nix-command flakes' github:nix-community/nixos-anywhere -- --flake .#h1 --target-host root@IP --build-on remote
-      nixosConfigurations.h1 = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        specialArgs = {
-          inherit inputs;
+        # Use this for all other targets
+        # nixos-anywhere --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix <hostname>
+        generic = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            disko.nixosModules.disko
+            ./configuration.nix
+            ./hardware-configuration.nix
+          ];
         };
-        modules = project.systems.nixos.h1._module.args.modules;
-      };
 
-      # Use this for all other targets
-      # nixos-anywhere --flake .#generic --generate-hardware-config nixos-generate-config ./hardware-configuration.nix <hostname>
-      nixosConfigurations.generic = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-          ./hardware-configuration.nix
-        ];
-      };
-
-      # Slightly experimental: Like generic, but with nixos-facter (https://github.com/numtide/nixos-facter)
-      # nixos-anywhere --flake .#generic-nixos-facter --generate-hardware-config nixos-facter facter.json <hostname>
-      nixosConfigurations.generic-nixos-facter = nixpkgs.lib.nixosSystem {
-        system = "x86_64-linux";
-        modules = [
-          disko.nixosModules.disko
-          ./configuration.nix
-          nixos-facter-modules.nixosModules.facter
-          {
-            config.facter.reportPath =
-              if builtins.pathExists ./facter.json then
-                ./facter.json
-              else
-                throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
-          }
-        ];
+        # Slightly experimental: Like generic, but with nixos-facter (https://github.com/numtide/nixos-facter)
+        # nixos-anywhere --flake .#generic-nixos-facter --generate-hardware-config nixos-facter facter.json <hostname>
+        generic-nixos-facter = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          modules = [
+            disko.nixosModules.disko
+            ./configuration.nix
+            nixos-facter-modules.nixosModules.facter
+            {
+              config.facter.reportPath =
+                if builtins.pathExists ./facter.json then
+                  ./facter.json
+                else
+                  throw "Have you forgotten to run nixos-anywhere with `--generate-hardware-config nixos-facter ./facter.json`?";
+            }
+          ];
+        };
       };
     };
 }
--- hosts/dns/networking.nix
+++ hosts/dns/networking.nix
@@ -2,25 +2,14 @@
 {
   networking = {
     # hostName = "pi-dns";
+    defaultGateway = {
+      address = "192.168.178.1";
+      interface = "end0";
+    };
     interfaces.end0.ipv4.addresses = [{
       address = "192.168.178.42";
       prefixLength = 24;
     }];
+    nameservers = [ "192.168.178.1" ];
   };
-
-
-  # networking = {
-  #   hostName = "pi-dns";
-  #   defaultGateway = {
-  #     address = "192.168.178.1";
-  #     interface = "end0";
-  #   };
-  #   nameservers = [ "192.168.178.1" ];
-  #   inferfaces.end0 = {
-  #     ipv4.addresses = [{
-  #       address = "192.168.178.42";
-  #       prefixLength = 24;
-  #     }];
-  #   };
-  # };
 }
