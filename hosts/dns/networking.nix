2d9199d5f22898fe64fc89ff5cd594c51d684824
erdfern <git@mail.erdfern.com>
2025-10-09 15:06:28 +0200

    --wip--

--- hosts/dns/networking.nix
+++ hosts/dns/networking.nix
@@ -1,17 +1,25 @@
 { ... }:
 {
-  networking = {
-    hostName = "pi-dns";
-    defaultGateway = {
-      address = "192.168.178.1";
-      interface = "end0";
-    };
-    nameservers = [ "192.168.178.1" ];
-    inferfaces.end0 = {
+  networking.interfaces = {
+    end0 = {
       ipv4.addresses = [{
         address = "192.168.178.42";
         prefixLength = 24;
       }];
     };
   };
+  # networking = {
+  #   hostName = "pi-dns";
+  #   defaultGateway = {
+  #     address = "192.168.178.1";
+  #     interface = "end0";
+  #   };
+  #   nameservers = [ "192.168.178.1" ];
+  #   inferfaces.end0 = {
+  #     ipv4.addresses = [{
+  #       address = "192.168.178.42";
+  #       prefixLength = 24;
+  #     }];
+  #   };
+  # };
 }
