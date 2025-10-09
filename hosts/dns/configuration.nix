da9b147f2443e9ed0c66cdcdc882ccb5e70373e7
erdfern <git@mail.erdfern.com>
2025-10-09 20:06:05 +0200

    --wip--

--- hosts/dns/configuration.nix
+++ hosts/dns/configuration.nix
@@ -22,10 +22,10 @@
   kor.basic-utils = false;
 
   users.users = {
-    root.openssh.authorizedKeys.keys = [ ];
+    root.openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFC/7GNB7BzQQP9Au/tKFPSKOiyL18HJaUwCqy/sSdrf j@kor" ];
     root.initialHashedPassword = "$6$tdKqjdDlHB55JViJ$yZmv/XchjdrGN3iH79oVSe1cLYsKYXsiP2kSPMt1hN84yvY.orRR6Gujx.qNLVDzfqaKiNWhtSiojMWku3ETg0";
     ${me.user} = {
-      openssh.authorizedKeys.keys = [ ];
+      openssh.authorizedKeys.keys = [ "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFC/7GNB7BzQQP9Au/tKFPSKOiyL18HJaUwCqy/sSdrf j@kor" ];
       initialHashedPassword = "$6$tdKqjdDlHB55JViJ$yZmv/XchjdrGN3iH79oVSe1cLYsKYXsiP2kSPMt1hN84yvY.orRR6Gujx.qNLVDzfqaKiNWhtSiojMWku3ETg0";
     };
   };
