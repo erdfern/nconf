226ac39d89e6c293b840cd9278b3f32ac7d3f64b
erdfern <git@mail.erdfern.com>
2025-10-18 16:57:10 +0200

    --wip--

--- nilla.nix
+++ nilla.nix
@@ -120,16 +120,18 @@ in
     systems.nixos.kor = {
       # modules = [ config.modules.nixos.lix ];
       modules = [
-        ({ ... }: {
-          nixpkgs.overlays = [
-            (final: prev: {
-              inherit (prev.lixPackageSets.stable)
-                nixpkgs-review
-                nix-eval-jobs
-                nix-fast-build
-                colmena;
-            })
-          ];
+        ({ pkgs, ... }: {
+          nix.package = pkgs.lixPackageSets.stable.lix;
+          # NOTE 16.10.25 the nixpkgs lix package does this (and more) by itself now!
+          # nixpkgs.overlays = [
+          #   (final: prev: {
+          #     inherit (prev.lixPackageSets.stable)
+          #       nixpkgs-review
+          #       nix-eval-jobs
+          #       nix-fast-build
+          #       colmena;
+          #   })
+          # ];
         })
       ];
     };
