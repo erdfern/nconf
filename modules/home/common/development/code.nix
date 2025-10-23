6133b11daf13830041956b8fdef60e04dfef3a0b
erdfern <git@mail.erdfern.com>
2025-10-23 21:50:42 +0200

    --wip--

--- modules/home/common/development/code.nix
+++ modules/home/common/development/code.nix
@@ -12,7 +12,8 @@ in
         (oldAttrs: {
           src = (builtins.fetchTarball {
             url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-            sha256 = "sha256-wjVw3n9BknUVjNDlbrTa8Wf3aaIj8+p4iunuLW8i23Y=";
+            # # sha256 = "sha256-wjVw3n9BknUVjNDlbrTa8Wf3aaIj8+p4iunuLW8i23Y=";
+            sha256 = "sha256-zl4zl1HepayCQ7aFt+dGN6EHLLvaJlCdGdym4SsdNwk=";
           });
           version = "latest";
 
