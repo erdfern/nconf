0bdd9097004ba317d8e9f4edb7a12652e8a1fa4f
erdfern <git@mail.erdfern.com>
2025-10-27 14:44:57 +0100

    --wip--

--- me.nix
+++ me.nix
@@ -3,6 +3,7 @@
   email = "jay@erdfern.dev";
   domain = "";
   ssh.pubKeys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFC/7GNB7BzQQP9Au/tKFPSKOiyL18HJaUwCqy/sSdrf j@kor"
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIGbfWj2zAADpQBzrqydUq40755qifLmzOMp3We32hs7hAAAAE3NzaDpsYXguZXJkZmVybi5kZXY= ssh:lax.erdfern.dev"
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINbSc82sYDBWblZ53C9kGWBZhApXu1x65/z/MLJ40OgNAAAAFnNzaDpzdHJpY3QuZXJkZmVybi5kZXY= ssh:strict.erdfern.dev"
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMME0ww1s7x37uSONLhXUPVjnXbErrxEC432Fp9l8qhzAAAAFnNzaDpsYXgubmsuZXJkZmVybi5kZXY= ssh:lax.nk.erdfern.dev"
