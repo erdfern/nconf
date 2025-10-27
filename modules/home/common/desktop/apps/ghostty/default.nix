f830e9287e57f782ee18602008dbcaf0a9c416c2
erdfern <git@mail.erdfern.com>
2025-10-27 16:30:38 +0100

    --wip--

--- modules/home/common/desktop/apps/ghostty/default.nix
+++ modules/home/common/desktop/apps/ghostty/default.nix
@@ -9,6 +9,16 @@ in
 
   config = {
     programs = lib.mkIf cfg.enable {
+      # enable systemd service, provided by ghostty package (~/.nix-profile/share/systemd/user/app-com.mitchellh.ghostty.service)
+      systemd.user.services."ghosttyineeedyou" = {
+        Unit = {
+          Description = "Express a deep longing for the ghostty service.. will it heed my call?";
+          # Documentation = [ "" ];
+          After = [ "graphical-session.target" ];
+          Requires = "app-com.mitchellh.ghostty.service";
+        };
+        Install.WantedBy = [ "graphical-session.target" ];
+      };
       ghostty = {
         enable = cfg.enable;
         # installVimSyntax = true;
