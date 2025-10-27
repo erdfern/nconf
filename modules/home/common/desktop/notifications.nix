d71912f99094c03ab3c21872109489fa0201679f
erdfern <git@mail.erdfern.com>
2025-10-27 18:10:15 +0100

    --wip--

--- modules/home/common/desktop/notifications.nix
+++ modules/home/common/desktop/notifications.nix
@@ -32,9 +32,9 @@ in
           background = hexcolor "base";
 
           title-color = hexcolor "text";
-          title-font = "Hack Nerd Font:slant=italic:size=18";
+          title-font = "Geist Mono Nerd Font:weight=bold:size=24";
           summary-color = hexcolor "subtext0";
-          summary-font = "Hack Nerd Font:weight=bold:size=14";
+          summary-font = "Hack Nerd Font:size=16";
           body-color = hexcolor "text";
           body-font = "DroidSansM Nerd Font";
         };
