3fa6ba4e071628335c261cddcc7ca58296d8a848
erdfern <git@mail.erdfern.com>
2025-10-27 14:44:07 +0100

    --wip--

--- me.nix
+++ me.nix
@@ -6,6 +6,7 @@
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIGbfWj2zAADpQBzrqydUq40755qifLmzOMp3We32hs7hAAAAE3NzaDpsYXguZXJkZmVybi5kZXY= ssh:lax.erdfern.dev"
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAINbSc82sYDBWblZ53C9kGWBZhApXu1x65/z/MLJ40OgNAAAAFnNzaDpzdHJpY3QuZXJkZmVybi5kZXY= ssh:strict.erdfern.dev"
     "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMME0ww1s7x37uSONLhXUPVjnXbErrxEC432Fp9l8qhzAAAAFnNzaDpsYXgubmsuZXJkZmVybi5kZXY= ssh:lax.nk.erdfern.dev"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIECe8DRTjjxw5ica26wmeeXFh3586ask6eL9NxwUsDI3 lk@kor-t14"
   ];
   gpg = {
     signKey = "F6C79FF4E1D142EE";
