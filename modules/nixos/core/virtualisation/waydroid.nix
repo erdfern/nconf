7ecd9da8c7f839bcbab9864f0f500d20a5a42f9d
erdfern <git@mail.erdfern.com>
2025-11-13 16:09:24 +0100

    --wip--

--- modules/nixos/core/virtualisation/waydroid.nix
+++ modules/nixos/core/virtualisation/waydroid.nix
@@ -6,6 +6,10 @@ in
   options.kor.virtualisation.waydroid.enable = lib.mkEnableOption "Waydroid Android emulator";
 
   config = lib.mkIf cfg.enable {
+    # TODO handle this differently! this module should not change the firewall by itself. atm, though, iptables doesn't work for me with waydroid
+    networking.nftables.enable = true;
+    virtualisation.waydroid.package = pkgs.waydroid-nftables;
+
     virtualisation.waydroid.enable = true;
     environment.systemPackages = [
       pkgs.waydroid-helper
