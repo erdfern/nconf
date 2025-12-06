39e5f13998f3bec05841cb85e01d3c0a55c72398
erdfern <git@mail.erdfern.com>
2025-12-06 17:00:00 +0100

    --wip--

--- hosts/kor/configuration.nix
+++ hosts/kor/configuration.nix
@@ -32,54 +32,61 @@
   kor.virtualisation.containers.enable = true;
   kor.virtualisation.waydroid.enable = true;
 
+
   services.deluge = {
     enable = true;
     web.enable = false;
   };
 
-  # environment.systemPackages = [ pkgs.zoom-us ];
-  # programs.obs-studio = {
-  #   enable = true;
-  #   enableVirtualCamera = true;
-  #   plugins = with pkgs.obs-studio-plugins; [
-  #     droidcam-obs
-  #   ];
-  # };
-  # programs.adb.enable = true;
-  # # users.users.j.extraGroups = [ "adbusers" ];
-  # services.udev.packages = [ pkgs.android-udev-rules ];
+  environment.systemPackages = [
+    pkgs.inskscape
+    # (pkgs.inkscape-with-extensions.override {
+    #   inkscapeExtensions = with pkgs.inkscape-extensions; [];
+    # })
+  ]
+    # environment.systemPackages = [ pkgs.zoom-us ];
+    # programs.obs-studio = {
+    #   enable = true;
+    #   enableVirtualCamera = true;
+    #   plugins = with pkgs.obs-studio-plugins; [
+    #     droidcam-obs
+    #   ];
+    # };
+    # programs.adb.enable = true;
+    # # users.users.j.extraGroups = [ "adbusers" ];
+    # services.udev.packages = [ pkgs.android-udev-rules ];
 
-  # networking.firewall.enable = false;
+    # networking.firewall.enable = false;
 
-  # networking.hostId = "7c238412";
-  # networking.interfaces = {
-  #   enp9s0 = {
-  #     useDHCP = true;
-  #     wakeOnLan.enable = true;
-  #     wakeOnLan.policy = [ "magic" ];
-  #   };
-  # };
+    # networking.hostId = "7c238412";
+    # networking.interfaces = {
+    #   enp9s0 = {
+    #     useDHCP = true;
+    #     wakeOnLan.enable = true;
+    #     wakeOnLan.policy = [ "magic" ];
+    #   };
+    # };
 
-  # boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
-  # boot.kernelPackages = pkgs.linuxKernel.kernels.linux_zen;
-  # boot.kernelPackages = pkgs.linuxPackages_zen;
+    # boot.kernelPackages = pkgs.linuxPackages_xanmod_latest;
+    # boot.kernelPackages = pkgs.linuxKernel.kernels.linux_zen;
+    # boot.kernelPackages = pkgs.linuxPackages_zen;
 
-  services.openssh = {
-    enable = true;
-  };
+    services.openssh = {
+  enable = true;
+};
 
-  hardware.logitech.wireless.enable = true;
-  hardware.logitech.wireless.enableGraphical = true;
+hardware.logitech.wireless.enable = true;
+hardware.logitech.wireless.enableGraphical = true;
 
-  users.mutableUsers = false;
-  users.users.root.initialHashedPassword = "$6$F2VMMSRv8pG5wHRw$HVtjknqzelzHPaIM6a4gmeQpYT4CHlhClVkfjU5hjItM41LOIwzy7M9iOMgWdeTOCB8ccIWiRY/v0.1MexDQu.";
-  users.users.${me.user} = {
-    initialHashedPassword = "$6$F2VMMSRv8pG5wHRw$HVtjknqzelzHPaIM6a4gmeQpYT4CHlhClVkfjU5hjItM41LOIwzy7M9iOMgWdeTOCB8ccIWiRY/v0.1MexDQu.";
-    # TEMP
-    extraGroups = [
-      "adbusers"
-    ];
-  };
+users.mutableUsers = false;
+users.users.root.initialHashedPassword = "$6$F2VMMSRv8pG5wHRw$HVtjknqzelzHPaIM6a4gmeQpYT4CHlhClVkfjU5hjItM41LOIwzy7M9iOMgWdeTOCB8ccIWiRY/v0.1MexDQu.";
+users.users.${me.user} = {
+initialHashedPassword = "$6$F2VMMSRv8pG5wHRw$HVtjknqzelzHPaIM6a4gmeQpYT4CHlhClVkfjU5hjItM41LOIwzy7M9iOMgWdeTOCB8ccIWiRY/v0.1MexDQu.";
+# TEMP
+extraGroups = [
+"adbusers"
+];
+};
 
-  system.stateVersion = "25.11";
+system.stateVersion = "25.11";
 }
