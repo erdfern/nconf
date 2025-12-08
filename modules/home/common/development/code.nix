65b2a93076f6bfcc277478153128375a73990d2f
erdfern <git@mail.erdfern.com>
2025-12-08 18:01:45 +0100

    --wip--

--- modules/home/common/development/code.nix
+++ modules/home/common/development/code.nix
@@ -1,4 +1,4 @@
-{ pkgs, lib, config, ... }:
+{ pkgs, lib, config, inputs, ... }:
 
 let
   cfg = config.kor.development.vscode;
@@ -10,10 +10,12 @@ in
     home.packages = [
       ((pkgs.vscode.override { isInsiders = true; }).overrideAttrs
         (oldAttrs: {
-          src = (builtins.fetchTarball {
-            url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-            sha256 = "sha256-L0UEE7pEA05cMj7SCmWqtvfRcW+BZshmVx2shLw9Gvg=";
-          });
+          # src = (builtins.fetchTarball {
+          #   url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
+          #   # sha256 = "sha256-L0UEE7pEA05cMj7SCmWqtvfRcW+BZshmVx2shLw9Gvg=";
+          #   sha256 = lib.fakeSha256;
+          # });
+          src = inputs.vscode-insider.src;
           version = "latest";
 
           buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
