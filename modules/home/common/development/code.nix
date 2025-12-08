637fe306db8ee1bc37e4e4c46bd0c0f7aed5cdc3
erdfern <git@mail.erdfern.com>
2025-12-08 18:14:51 +0100

    --wip--

--- modules/home/common/development/code.nix
+++ modules/home/common/development/code.nix
@@ -10,15 +10,15 @@ in
     home.packages = [
       ((pkgs.vscode.override { isInsiders = true; }).overrideAttrs
         (oldAttrs: {
-          # src = (builtins.fetchTarball {
-          #   url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-          #   # sha256 = "sha256-L0UEE7pEA05cMj7SCmWqtvfRcW+BZshmVx2shLw9Gvg=";
-          #   sha256 = lib.fakeSha256;
-          # });
-          src = inputs.vscode-insider.src;
+          src = (builtins.fetchTarball {
+            url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
+            # sha256 = "sha256-L0UEE7pEA05cMj7SCmWqtvfRcW+BZshmVx2shLw9Gvg=";
+            sha256 = "sha256-aVtO1HyUD332wfPrshmsmCKKaGME2W0XMcXQMYzeuBg=";
+          });
+          # src = inputs.vscode-insider.src;
           version = "latest";
 
-          buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 ];
+          buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 pkgs.libsoup_3 pkgs.webkitgtk_4_1 ];
         }))
     ];
     # programs.vscode = {
