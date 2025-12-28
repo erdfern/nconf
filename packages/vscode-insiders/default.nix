bc67f067a2af8a330cb9d441bc722c1c7433af17
erdfern <git@mail.erdfern.com>
2025-12-28 14:39:19 +0100

    --wip--

--- packages/vscode-insiders/default.nix
+++ packages/vscode-insiders/default.nix
@@ -1,14 +1,16 @@
 { pkgs
-, inputs
+, lib
 }:
+# let
+# in
 (pkgs.vscode.override { isInsiders = true; }).overrideAttrs
   (oldAttrs: {
-    # src = (builtins.fetchTarball {
-    #   url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-    #   # sha256 = "sha256-L0UEE7pEA05cMj7SCmWqtvfRcW+BZshmVx2shLw9Gvg=";
-    #   sha256 = "sha256-aVtO1HyUD332wfPrshmsmCKKaGME2W0XMcXQMYzeuBg=";
-    # });
-    src = inputs.vscode-insider.src;
+    src = (fetchTarball {
+      url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
+      # sha256 = "sha256-L0UEE7pEA05cMj7SCmWqtvfRcW+BZshmVx2shLw9Gvg=";
+      sha256 = lib.fakeSha256;
+    });
+    # src = inputs.vscode-insider.src;
     version = "latest";
 
     buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 pkgs.libsoup_3 pkgs.webkitgtk_4_1 ];
