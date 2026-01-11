f64e134d44fc9e5f41fe20ec8ee26f1b390ee63a
erdfern <git@mail.erdfern.com>
2026-01-11 14:04:49 +0100

    --wip--

--- packages/vscode-insiders/default.nix
+++ packages/vscode-insiders/default.nix
@@ -9,7 +9,7 @@
       url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
       # sha256 = lib.fakeSha256;
       # sha256 = "sha256-YbWlrSABmVwBd9aVrna7yynCuz7v8luhVGgMnvpQDPo=";
-      sha256 = "sha256-L0UEE7pEA05cMj7SCmWqtvfRcW+BZshmVx2shLw9Gvg=";
+      sha256 = "sha256-YbWlrSABmVwBd9aVrna7yynCuz7v8luhVGgMnvpQDPo=";
     });
     # src = inputs.vscode-insider.src;
     version = "latest";
