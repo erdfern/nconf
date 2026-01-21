13c638dbabae64d91836bd86e29f9ccbad60fc75
erdfern <git@mail.erdfern.com>
2026-01-21 11:02:20 +0100

    --wip--

--- modules/home/common/desktop/apps/firefox/extensions/default.nix
+++ modules/home/common/desktop/apps/firefox/extensions/default.nix
@@ -3,7 +3,7 @@
 let
   # Search extension names with below command:
   # nix flake show --json "gitlab:rycee/nur-expressions?dir=pkgs/firefox-addons" --all-systems | jq -r '.packages."x86_64-linux" | keys[]' | rg QUERY
-  # ryceeAddons = with inputs.firefox-addons.result.packages.${pkgs.system};
+  # ryceeAddons = with inputs.firefox-addons.result.packages.${pkgs.stdenv.hostPlatform.system};
   # ryceeAddons = with inputs.firefox-addons.result; # only x86_64 for now
   ryceeAddons = with (inputs.rycee-nur-expressions.result { pkgs = pkgs; }).firefox-addons; [
     ublock-origin
