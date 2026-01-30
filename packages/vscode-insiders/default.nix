270e8ec9606b515983197aa5a612173a8ef18c38
erdfern <git@mail.erdfern.com>
2026-01-30 23:47:04 +0100

    --wip--

--- packages/vscode-insiders/default.nix
+++ packages/vscode-insiders/default.nix
@@ -9,10 +9,10 @@
       url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
       # sha256 = lib.fakeSha256;
       # sha256 = "sha256-L0UEE7pEA05cMj7SCmWqtvfRcW+BZshmVx2shLw9Gvg=";
-      sha256 = "sha256-YbWlrSABmVwBd9aVrna7yynCuz7v8luhVGgMnvpQDPo=";
+      sha256 = lib.fakeSha256;
     });
     # src = inputs.vscode-insider.src;
-    version = "latest";
+    version = "01.30.26";
 
     buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 pkgs.libsoup_3 pkgs.webkitgtk_4_1 ];
   })
