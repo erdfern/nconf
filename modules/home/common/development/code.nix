0c595237edfec3c29f87730f401aabcc9e6e560e
erdfern <git@mail.erdfern.com>
2026-01-31 00:00:23 +0100

    --wip--

--- modules/home/common/development/code.nix
+++ modules/home/common/development/code.nix
@@ -8,19 +8,8 @@ in
 
   config = lib.mkIf cfg.enable {
     home.packages = [
-      pkgs.vscode-insiders
-      # (pkgs.vscode.override { isInsiders = true; }).overrideAttrs
-      #   (oldAttrs: {
-      #     # src = (builtins.fetchTarball {
-      #     #   url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-      #     #   # sha256 = "sha256-L0UEE7pEA05cMj7SCmWqtvfRcW+BZshmVx2shLw9Gvg=";
-      #     #   sha256 = "sha256-aVtO1HyUD332wfPrshmsmCKKaGME2W0XMcXQMYzeuBg=";
-      #     # });
-      #     src = inputs.vscode-insider.src;
-      #     version = "latest";
-
-      #     buildInputs = oldAttrs.buildInputs ++ [ pkgs.krb5 pkgs.libsoup_3 pkgs.webkitgtk_4_1 ];
-      #   })
+      # pkgs.vscode-insiders
+      inputs.code-insiders-flake.result.packages.x86_64-linux.vscode-insiders
     ];
     # programs.vscode = {
     #   enable = true;
