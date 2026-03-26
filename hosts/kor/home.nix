f22b63fbb58457dd3e773373bb5d59a0880b4526
erdfern <git@mail.erdfern.com>
2026-03-26 12:34:34 +0100

    --wip--

--- hosts/kor/home.nix
+++ hosts/kor/home.nix
@@ -26,7 +26,8 @@
   home.packages = [
     # inputs.devenv.result.packages.x86_64-linux.default
     pkgs.devenv
-    pkgs.freecad
+    # pkgs.freecad
+    pkgs.freecad-git
     # pkgs.seamly2d
     # pkgs.valentina-git
   ];
