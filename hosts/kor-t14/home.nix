fe7fde5aaf9bc1daebff5abf77cc4667cfec28ea
erdfern <git@mail.erdfern.com>
2026-04-14 11:06:14 +0200

    --wip--

--- hosts/kor-t14/home.nix
+++ hosts/kor-t14/home.nix
@@ -51,7 +51,7 @@
     freecad
     openscad-unstable
     openscad-lsp
-    orca-slicer
+    # orca-slicer
     # npins
     # inputs.nilla-cli.result.packages.nilla-cli.result.x86_64-linux
     # inputs.nilla-utils.result.packages.nilla-utils-plugins.result.x86_64-linux
