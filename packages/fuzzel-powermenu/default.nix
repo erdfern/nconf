d48ebf8955acaf9767790d8198e975587b777a16
erdfern <git@mail.erdfern.com>
2026-04-23 10:31:22 +0200

    --wip--

--- packages/fuzzel-powermenu/default.nix
+++ packages/fuzzel-powermenu/default.nix
@@ -3,13 +3,15 @@
   # ... TODO
 }:
 writeShellScriptBin "fuzzel-powermenu" ''
-  SELECTION="$(printf "1 - Lock\n2 - Suspend\n3 - Log out\n4 - Reboot\n5 - Reboot to UEFI\n6 - Shutdown" | fuzzel --dmenu -l 6 -p "Power Menu: ")"
+  SELECTION="$(printf "1 - Shutdown\n2 - Suspend\n3 - Lock\n4 - Log out\n5 - Reboot\n6 - Reboot to UEFI" | fuzzel --dmenu -l 6 -p "Power Menu: ")"
 
   case $SELECTION in
-  	*"Lock")
-  		pidof hyprlock || hyprlock;;
+  	*"Shutdown")
+  		systemctl poweroff;;
   	*"Suspend")
   		systemctl suspend;;
+  	*"Lock")
+  		pidof hyprlock || hyprlock;;
   	*"Log out")
   		# loginctl terminate-user "";;
   		uwsm stop;;
@@ -17,7 +19,5 @@ writeShellScriptBin "fuzzel-powermenu" ''
   		systemctl reboot;;
   	*"Reboot to UEFI")
   		systemctl reboot --firmware-setup;;
-  	*"Shutdown")
-  		systemctl poweroff;;
   esac
 ''
