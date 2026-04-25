a801cd8cad84a8689cf52cbf7d473b8ba3d0d73e
erdfern <git@mail.erdfern.com>
2026-04-25 18:52:52 +0200

    --wip--

--- packages/vscode-insiders/default.nix
+++ packages/vscode-insiders/default.nix
@@ -7,8 +7,8 @@
   (oldAttrs: {
     src = (fetchTarball {
       url = "https://code.visualstudio.com/sha/download?build=insider&os=linux-x64";
-      sha256 = lib.fakeSha256;
-      # sha256 = "sha256-YbWlrSABmVwBd9aVrna7yynCuz7v8luhVGgMnvpQDPo=";
+      # sha256 = lib.fakeSha256;
+      sha256 = "sha256:0ym11zspj2l2pmjfg6fnjk39kassnzk20mghrchndf7hblyzmx2l";
     });
     # src = inputs.vscode-insider.src;
     version = "25.04.26";
