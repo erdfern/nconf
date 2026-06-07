d2780a7b942b9e6d389fca0a314508fd1548ba62
erdfern <git@mail.erdfern.com>
2026-06-07 12:26:39 +0200

    --wip--

--- hosts/kor/disk-config.nix
+++ hosts/kor/disk-config.nix
@@ -3,7 +3,7 @@
   disko.devices = {
     disk = {
       primary = {
-        device = "/dev/by-id/xxx";
+        device = "/dev/by-id/nvme-Samsung_SSD_970_PRO_1TB_S462NF0MA06388N_1";
         type = "disk";
         content = {
           type = "gpt";
