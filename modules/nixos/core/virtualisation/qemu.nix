feb0db6989424308b1db08e52facb4a0d9402baa
erdfern <git@mail.erdfern.com>
2026-06-19 15:02:10 +0200

    --wip--

--- modules/nixos/core/virtualisation/qemu.nix
+++ modules/nixos/core/virtualisation/qemu.nix
@@ -21,15 +21,6 @@ in
         package = pkgs.qemu_kvm;
         runAsRoot = true;
         swtpm.enable = true;
-        ovmf = {
-          enable = true;
-          packages = [
-            (pkgs.OVMF.override {
-              secureBoot = true;
-              tpmSupport = true;
-            }).fd
-          ];
-        };
       };
 
       # hooks = {
