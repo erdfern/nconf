74a0bf2f4c6779e07fa38e76f77fc108e2c3e885
erdfern <git@mail.erdfern.com>
2026-06-19 14:47:04 +0200

    --wip--

--- hosts/kor/configuration.nix
+++ hosts/kor/configuration.nix
@@ -37,7 +37,7 @@
 
   # kor.virtualisation.qemu.enable = true;
   kor.virtualisation.containers.enable = true;
-  # kor.virtualisation.waydroid.enable = true;
+  kor.virtualisation.waydroid.enable = true;
 
 
   services.deluge = {
