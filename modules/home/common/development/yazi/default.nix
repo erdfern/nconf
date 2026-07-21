04679cbd2429b905d64e4541a1b2b3f894366cce
erdfern <git@mail.erdfern.com>
2026-07-21 13:02:17 +0200

    --wip--

--- modules/home/common/development/yazi/default.nix
+++ modules/home/common/development/yazi/default.nix
@@ -2,15 +2,15 @@
 {
   programs.yazi = {
     enable = true;
-    enableFishIntegration = true;
-    enableBashIntegration = true;
+    # enableFishIntegration = true;
+    # enableBashIntegration = true;
     # enableNushellIntegration = true;
     shellWrapperName = "yy"; # "y" is default
   };
 
-  home.file = {
-    ".config/yazi/yazi.toml".source = ./yazi.toml;
-    ".config/yazi/keymap.toml".source = ./keymap.toml;
-    # ".config/yazi/theme.toml".source = ./theme.toml;
-  };
+  # home.file = {
+  #   ".config/yazi/yazi.toml".source = ./yazi.toml;
+  #   ".config/yazi/keymap.toml".source = ./keymap.toml;
+  # # ".config/yazi/theme.toml".source = ./theme.toml;
+  # };
 }
